1bd36a5fa1ca77d0ab973450f96c15871aee714c
anshad <anshad.musafir@gmail.com>
2022-10-18 20:45:19 +0400

    Add theme and login screen

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
 import { createRoot } from 'react-dom/client';
 import axios from 'axios';
 
+import './styles/style.scss';
+import './styles/plugins.scss';
+import './styles/style.react.scss';
+
 import { setupAxios } from './modules/auth';
 import { AppRoutes } from './routes/AppRoutes';
 
--- src/modules/auth/AuthPage.tsx
+++ src/modules/auth/AuthPage.tsx
@@ -1,3 +1,16 @@
-const AuthPage = () => <p>Auth page</p>;
+import { Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import ForgotPassword from './components/ForgotPassword';
+import { AuthLayout } from './AuthLayout';
+
+const AuthPage = () => (
+  <Routes>
+    <Route element={<AuthLayout />}>
+      <Route path='login' element={<Login />} />
+      <Route path='forgot-password' element={<ForgotPassword />} />
+      <Route index element={<Login />} />
+    </Route>
+  </Routes>
+);
 
 export { AuthPage };
